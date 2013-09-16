097d45a4f972c93e5308be12421e66cd6d4f3aac
Asbjørn A. Fellinghaug <asbjorn@fellinghaug.com>
2013-09-16 14:38:55 +0200

    JPO to be used in the ListView

--- CanteenAppProject/CanteenApp/src/main/java/com/example/canteenapp/model/CanteenItem.java
+++ CanteenAppProject/CanteenApp/src/main/java/com/example/canteenapp/model/CanteenItem.java
@@ -1,7 +1,45 @@
 package com.example.canteenapp.model;
 
+import java.io.Serializable;
+
 /**
  * Created by asbjorn on 9/16/13.
  */
-public class CanteenItem {
+public class CanteenItem implements Serializable {
+    private String id;
+    private String name;
+    private String link;
+
+    public CanteenItem() {
+    }
+
+    public CanteenItem(String id, String name, String link) {
+        this.setId(id);
+        this.setName(name);
+        this.setLink(link);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
