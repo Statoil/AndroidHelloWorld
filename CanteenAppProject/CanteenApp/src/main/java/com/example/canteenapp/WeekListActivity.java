06a0fc9106a919d5b7df672f02253460c34566bf
Asbjørn A. Fellinghaug <asbjorn@fellinghaug.com>
2013-09-16 14:40:28 +0200

    This activity is not yet done. Have deserialized the canteenitem and will use that to further start a new background thread to collect the weekly menu items.

--- CanteenAppProject/CanteenApp/src/main/java/com/example/canteenapp/WeekListActivity.java
+++ CanteenAppProject/CanteenApp/src/main/java/com/example/canteenapp/WeekListActivity.java
@@ -1,31 +1,50 @@
 package com.example.canteenapp;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.canteenapp.model.CanteenItem;
 import com.example.canteenapp.model.LunchMenuItem;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 public class WeekListActivity extends ListActivity {
 
     private LunchMenuAdapter lunchMenuAdapter;
+    public static final String TAG = WeekListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Intent intent = getIntent();
+        if(intent==null) {
+            Log.e(TAG, "Intent was null. Something is wrong!");
+            Toast.makeText(getApplicationContext(),
+                    "Something horribly happened..",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        CanteenItem canteenItem = (CanteenItem) intent.getSerializableExtra(MainActivity.CANTEEN_ITEM);
+
         lunchMenuAdapter = new LunchMenuAdapter();
         setListAdapter(lunchMenuAdapter);
 
-        //TODO: get this value sent to the activity (using intents)
-        String canteen = "Rotvoll";
-        lunchMenuAdapter.downloadLunchCourses(canteen);
+        // TODO: Start a new AsyncTask to download the courses for
+        // this particular "canteen"
+
+        //lunchMenuAdapter.downloadLunchCourses(canteen);
     }
 
     @Override
