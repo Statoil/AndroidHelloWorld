8814124ef28bd61526786847935f37ca49549846
Asbjørn A. Fellinghaug <asbjorn@fellinghaug.com>
2013-09-16 22:53:25 +0200

    working on the asynctask to fetch the menu for a canteen

--- CanteenAppProject/CanteenApp/src/main/java/com/example/canteenapp/task/WeekMenuFetcher.java
+++ CanteenAppProject/CanteenApp/src/main/java/com/example/canteenapp/task/WeekMenuFetcher.java
@@ -1,7 +1,35 @@
 package com.example.canteenapp.task;
 
+import android.os.AsyncTask;
+
+import com.example.canteenapp.model.LunchMenuItem;
+
+import java.util.ArrayList;
+
 /**
  * Created by asbjorn on 9/16/13.
  */
-public class WeekMenuFetcher {
+public class WeekMenuFetcher extends AsyncTask<Void, ArrayList<LunchMenuItem>, ArrayList<LunchMenuItem>> {
+    public static final String TAG = WeekMenuFetcher.class.getSimpleName();
+    public static final String url = "http://canteen-app.herokuapp.com/canteens/";
+
+    WeekMenuFetcher() {
+
+    }
+
+    private final String getUrl(String canteenName) {
+        return url+canteenName;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected ArrayList<LunchMenuItem> doInBackground(Void... voids) {
+        return null;
+    }
+
+
 }
