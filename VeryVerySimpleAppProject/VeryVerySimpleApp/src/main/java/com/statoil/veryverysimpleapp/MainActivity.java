45ba48f81e715a9473c02cea531a38c9da2923a9
Asbjørn A. Fellinghaug <asbjorn@fellinghaug.com>
2013-09-18 08:16:36 +0200

    added a seekbar and a listner/changer

--- VeryVerySimpleAppProject/VeryVerySimpleApp/src/main/java/com/statoil/veryverysimpleapp/MainActivity.java
+++ VeryVerySimpleAppProject/VeryVerySimpleApp/src/main/java/com/statoil/veryverysimpleapp/MainActivity.java
@@ -9,10 +9,12 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
     public static final String TAG = MainActivity.class.getSimpleName();
+    private Animation rotate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +39,13 @@ public class MainActivity extends Activity {
         final Button animationBtn = (Button) findViewById(R.id.animationBtn);
 
         // Load the animation
-        final Animation rotate = AnimationUtils.loadAnimation(
+        rotate = AnimationUtils.loadAnimation(
                 getApplicationContext(),
                 R.anim.rotate);
+
+        final SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        SeekBarChangeListener seekBarListener = new SeekBarChangeListener(rotate);
+        seekBar.setOnSeekBarChangeListener(seekBarListener);
 
         // Attach a click listener for the Animation button.
         // Will start the animation defined in res/anim/rotate.xml
@@ -60,6 +66,31 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    private static class SeekBarChangeListener implements SeekBar.OnSeekBarChangeListener {
+        private final Animation rotateAnimation;
+        int progressChanged = 0;
+        public SeekBarChangeListener(Animation rotateAnimation) {
+            this.rotateAnimation = rotateAnimation;
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            progressChanged = i;
+            Log.d(TAG, "progressChanged: "+i);
+            rotateAnimation.setDuration(i*100);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
     }
     
 }
